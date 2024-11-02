19cb7160bc13280ee07a7b7493f27eb09d529777
khalid-libdeh <khaledabulibdeh@gmail.com>
2024-11-02 18:16:55 +0200

    first-commit

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(props){
+
+    return(
+        <header className="flex justify-center bg-black text-white h-12 items-center">
+            <div className="flex content-center h-6 gap-2 text-center text-sm ">
+                <span >Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
+                <a href="" className="font-semibold">ShopNow</a>
+            </div>
+        </header>
+    )
+}
